31b740a9874d708469679423b4dcaf3c2b4c571e
ffxivfunsiteadm <ffxivfunsite.adm@gmail.com>
2025-05-02 15:48:47 +0900

    Add portfolio with FF14 and anime design

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+      section.style.opacity = 0;
+      setTimeout(() => {
+        section.style.transition = 'opacity 1s';
+        section.style.opacity = 1;
+      }, 500);
+    });
+  });
